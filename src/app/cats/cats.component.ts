94a4cca1c615a5efbf2cc9d5f2874c58e60e136c
Marat Safiulin <marat@backbase.com>
2019-03-07 18:03:11 +0200

    feat: search by button

--- src/app/cats/cats.component.ts
+++ src/app/cats/cats.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CatsService } from '../cats.service';
-import { Subscription, Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subscription, Subject, combineLatest, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cats',
@@ -9,40 +9,45 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./cats.component.css']
 })
 export class CatsComponent implements OnInit, OnDestroy {
-
+  searchTerm;
   catsList;
   getCatsSub: Subscription;
   searchTextSub: Subscription;
   searchTextChanged: Subject<string> = new Subject();
+  searchButton: Subject<string> = new Subject();
 
   constructor(private service: CatsService) { }
 
   ngOnInit() {
-    this.getCats('');
 
-    this.searchTextSub = this.searchTextChanged
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
-      )
-      .subscribe((term) => {
-        this.getCats(term);
-      });
+    this.searchTextSub = merge(
+        this.searchTextChanged
+        .pipe(
+          debounceTime(300),
+          distinctUntilChanged(),
+          filter((term) => term.length >= 3)
+        ),
+          this.searchButton)
+        .pipe(
+          switchMap((term) => this.service.getCats(term))
+        )
+        .subscribe((data) => {
+          this.catsList = data;
+        });
+
+    this.searchTextChanged.next('');
   }
 
-  getCats(term) {
-    this.getCatsSub = this.service.getCats(term)
-      .subscribe((data) => {
-        this.catsList = data;
-    });
+  onSearch() {
+    this.searchButton.next(this.searchTerm);
   }
 
   ngOnDestroy() {
     if (this.getCatsSub) {
       this.getCatsSub.unsubscribe();
     }
-    if (this.getCatsSub) {
-      this.getCatsSub.unsubscribe();
+    if (this.searchTextSub) {
+      this.searchTextSub.unsubscribe();
     }
   }
 }
